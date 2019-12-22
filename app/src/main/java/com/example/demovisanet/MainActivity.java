ab73dda623af0999ce19a2b854525734541d1f65
ksandon <integraciones.visanet@necomplus.com>
2019-12-22 14:05:48 -0500

    Update SDK

--- app/src/main/java/com/example/demovisanet/MainActivity.java
+++ app/src/main/java/com/example/demovisanet/MainActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,58 +25,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
 
+    public String generatePurchaseNumber() {
+        Long time = (new Date().getTime())/100000;
+        return time.toString();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //Busco el boton y lo assigno a una variable
-        Button botonComprar= findViewById(R.id.btncomprar);
-        //Luego le digo al boton que escuche cuando le dan click
+        Button botonComprar = findViewById(R.id.btncomprar);
+        final Switch sLogo = findViewById(R.id.sLogo);
+        final Switch sPagar = findViewById(R.id.sPagar);
+        final TextView tAmount = findViewById(R.id.tAmount);
+        final TextView tUserToken = findViewById(R.id.tUserToken);
+
         botonComprar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //cuando le des click al boton vas a mostrarle el formulario
-                //asegurate que estas son os usuarios y passwords
+                Log.i("EMAIL", tUserToken.getText().toString());
 
                 Map<String, Object> data = new HashMap<>();
 
                 data.put(VisaNet.VISANET_CHANNEL, VisaNet.Channel.MOBILE);
-
                 data.put(VisaNet.VISANET_COUNTABLE, true);
-
                 data.put(VisaNet.VISANET_USERNAME, "integraciones.visanet@necomplus.com");
-
                 data.put(VisaNet.VISANET_PASSWORD, "d5e7nk$M");
+                data.put(VisaNet.VISANET_MERCHANT, "100128038");
+                data.put(VisaNet.VISANET_PURCHASE_NUMBER, generatePurchaseNumber());
+                data.put(VisaNet.VISANET_AMOUNT, tAmount.getText());
+                data.put(VisaNet.VISANET_SHOW_AMOUNT, sPagar.isChecked());
+                data.put(VisaNet.VISANET_USER_TOKEN, tUserToken.getText().toString());
 
-                data.put(VisaNet.VISANET_MERCHANT, "342062522");
-
-                data.put(VisaNet.VISANET_PURCHASE_NUMBER, "1799");
-
-                data.put(VisaNet.VISANET_AMOUNT, "15.22");
-
-//Registro de MDDs complementarios (Ingreso opcional)
+                // Registro de MDD's
                 HashMap<String, String> MDDdata = new HashMap<String, String>();
-                MDDdata.put("19", "LIM");
-                MDDdata.put("20", "AQP");
-                MDDdata.put("21", "AFKI345");
-                MDDdata.put("94", "ABC123DEF");
+                // Consultar envío de MDD's
+                MDDdata.put("4", "mail@domain.com");
 
                 data.put(VisaNet.VISANET_MDD, MDDdata);
 
-                //Especificación de endpoint (Ingreso obligatorio)
-                data.put(VisaNet.VISANET_END_POINT_URL, "https://apitestenv.vnforapps.com/");
+                // Especificación de endpoint (Ingreso obligatorio)
+                data.put(VisaNet.VISANET_ENDPOINT_URL, "https://apitestenv.vnforapps.com/");
 
-                //Personalización (Ingreso opcional)
                 VisaNetViewAuthorizationCustom custom = new VisaNetViewAuthorizationCustom();
 
                 //Personalización 1: Configuración del nombre del comercio en el formulario de pago
-                custom.setLogoTextMerchant(true);
-                custom.setLogoTextMerchantText("Text logo");
-                custom.setLogoTextMerchantTextColor(R.color.visanet_black);
-                custom.setLogoTextMerchantTextSize(20);
+                custom.setLogoTextMerchant(!sLogo.isChecked());
+                    // Mostrar Nombre Del Comercio
+                    custom.setLogoTextMerchantText("Merchant Name");
+                    custom.setLogoTextMerchantTextColor(R.color.visanet_black);
+                    custom.setLogoTextMerchantTextSize(50);
+                    // Mostrar Logo Del Comercio
+                    custom.setLogoImage(R.drawable.logo);
 
                 //Personalización 2: Configuración del color del botón pagar en el formulario de pago
                 custom.setButtonColorMerchant(R.color.visanet_red);
@@ -86,14 +92,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-
     }
 
-    //ok ahora tenemos que hacer escuchar la respuesta en esta actividad
-    //pare eso tienes que override el listener de la actividad
-
+    // Respuesta
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
